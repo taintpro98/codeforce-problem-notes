bef2489b238c9e9bc505643c5cb36dc78e7ec23c
BatmanBruceWayne <taideptrai02@gmail.com>
2020-10-10 15:36:22 +0700

    done postman

--- bkcf/postman.cpp
+++ bkcf/postman.cpp
@@ -1,41 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct customer
-{
-	long x, m, r;	
-};
-
-bool cmp(customer a, customer b)
-{
-	return a.x < b.x;
-}
-
 int n;
 long k;
-customer a[1002];
+long long dist=0;
+typedef pair<int, int> pii;
+priority_queue<pii> a,b;
 
-long cal(int left, int right){
-    
+void sol(priority_queue<pii> &a){
+    long d = k;
+    while(!a.empty()){
+        pii c = a.top();
+        a.pop();
+        if(d == k){
+            dist += 2*c.first;
+        }
+        if (d<c.second){
+            c.second -= d;
+            a.push(c);
+            d = k;
+            
+        } else {
+            d -= c.second;
+        }
+    }
 }
 
 int main(){
-    freopen("in.txt", "r", stdin);
+    // freopen("bkcf/in.txt", "r", stdin);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> k;
-    for (int i=1; i<=n; i++) cin >> a[i].x >> a[i].m;
-    sort(a+1, a+n+1, cmp);
-    long dist = 0;
-    int pivot = 1001; // first place that x[pivot] > 0
     for (int i=1; i<=n; i++){
-        long q = a[i].m/k;
-        a[i].r = a[i].m%k;
-        if(a[i-1].x <= 0 && a[i].x > 0) pivot = i;
-        dist += 2*q*abs(a[i].x);
-    }
-    
+        int x, m;
+        cin >> x >> m;
+        long long q = m/k;
+        m = m%k;
+        dist += 2*q*abs(x);
+        if (!m) continue;
+        if(x >= 0){
+            a.push(make_pair(x, m));
+        } else {
+            b.push(make_pair(-x, m));
+        }
+    } 
+    sol(a);
+    sol(b);
+    cout << dist << endl;
 
     return 0;
 }
--- bkcf/test.cpp
+++ bkcf/test.cpp
@@ -1,8 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int x[1000];
-
+#define ll long long
+#define pii pair<int,int>
+#define pb push_back
+#define pob pop_back
+int n,k;
+ll ans = 0;
+priority_queue<pii> u,v;
+void solve(priority_queue<pii> &q){
+    int now = k;
+    while (q.size())
+    {
+        pii x = q.top();
+        q.pop();
+        if(now == k){
+            ans += 2 * x.first;
+        }
+        if(now >= x.second) {
+            now -= x.second;
+        }
+        else{
+            x.second -= now;
+            now = k;
+            q.push(x);
+        }
+    }
+}
 int main(){
-    cout << x[1];
+    freopen("bkcf/in.txt", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        int x,m;
+        cin >> x >> m;
+        ll num = m/k;
+        ans += num * 2 * abs(x);
+        if(m%k==0) continue;
+        if(x >= 0){
+            u.push(make_pair(x,m%k));
+        } else v.push(make_pair(-x,m%k));
+    }
+    solve(u);
+    solve(v);
+    cout << ans << endl;
     return 0;
 }
