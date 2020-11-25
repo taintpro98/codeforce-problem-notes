0f944048fbf6d3a2d045b54863d9fcfc4bf550d5
BatmanBruceWayne <taideptrai02@gmail.com>
2020-11-25 22:34:10 +0700

    done machine 100

---
+++ bkcf/machine.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=2e6+5;
+int n;
+// typedef pair<int, int> ii;
+// ii p[N];
+int t[N];
+int s[N];
+int maxs[N];
+
+int main(){
+    // freopen("bkcf/in.txt", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> s[i] >> t[i];
+    }
+    for(int i=1; i<=n; i++){
+        maxs[t[i]] = max(t[i] - s[i], maxs[t[i]]);
+    }
+    for(int i=1; i<N; i++){
+        maxs[i] = max(maxs[i-1], maxs[i]);
+    }
+    int ans = -1;
+    for(int i=1; i<=n; i++){
+        if(maxs[s[i]-1] > 0){
+            ans = max(ans, maxs[s[i] - 1] + t[i] - s[i]);
+        }
+    }
+    
+
+    cout << ans << endl;
+    return 0;
+}
